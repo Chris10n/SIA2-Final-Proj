f07c923e4e88241957368a5525e130fde5c6d3f0
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-10-15 14:05:58 +0800

    feat: add SMS messaging via HTTPSMS API feat: recipient eval by checking number length

---
+++ utils/sms_and_messaging/sms.py
@@ -0,0 +1,38 @@
+import requests
+import json
+from os import getenv
+from sys import exit
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def __validate_number(number):
+    try:
+        if len(number) > 13:
+            raise Exception("Recipient number is too long.")
+    except Exception as e:
+        print(f"Invalid phone number: {e}")
+        exit(1)
+
+
+def send_SMS(recipient, message):
+    __validate_number(recipient)
+
+    apiKey = getenv("HTTPSMS_API_KEY")
+    url = "https://api.httpsms.com/v1/messages/send"
+
+    headers = {
+        "x-api-key": apiKey,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "content": message,
+        "from": getenv("SMS_GATEWAY"),
+        "to": recipient,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(json.dumps(response.json(), indent=4))
